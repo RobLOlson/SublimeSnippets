c5d8acaacddfeb8e8849526a32d2c251bc4877e3
Rob L Olson <rob.louis@gmail.com>
2021-09-06 00:06:02 -0800

    improve ctrl+. selection expansion
    previously acted like ctrl+a once selection expanded over multiple lines;
    now successive applications will select higher levels of scope (as determined by indentation)

--- select_to_scope.py
+++ select_to_scope.py
@@ -1,8 +1,8 @@
+import re
+
 import sublime
 import sublime_plugin
-import pdb
 
-import re
 
 class BetterExpandSelectionCommand(sublime_plugin.TextCommand):
     def split_scope(self, pos):
@@ -25,12 +25,45 @@ class BetterExpandSelectionCommand(sublime_plugin.TextCommand):
             if expanded: selection.add(expanded)
 
     def get_expanded(self, region):
-        target = self.split_scope(region.begin())
-
+        target = self.split_scope(region.begin()) # starting index of passed selection
         while len(target):
             found = self.find_by_selector_containing(''.join(target), region.begin())
+            if "\n" in self.view.substr(found):
+                self.view.run_command("expand_selection", {"to": "indentation"})
+                return
             if found.contains(region) and not region.contains(found):
-                print(''.join(target))
                 return found
             else:
                 target.pop()
+
+
+# Original Version
+#
+#     def split_scope(self, pos):
+#         return re.findall(
+#             r'(?:^|\s+|\.)[^\s.]+',
+#             self.view.scope_name(pos)
+#         )
+
+#     def find_by_selector_containing(self, selector, point):
+#         return next(
+#             region
+#             for region in self.view.find_by_selector(selector)
+#             if region.contains(point)
+#         )
+
+#     def run(self, edit):
+#         selection = self.view.sel()
+#         for region in selection:
+#             expanded = self.get_expanded(region)
+#             if expanded: selection.add(expanded)
+
+#     def get_expanded(self, region):
+#         target = self.split_scope(region.begin())
+
+#         while len(target):
+#             found = self.find_by_selector_containing(''.join(target), region.begin())
+#             if found.contains(region) and not region.contains(found):
+#                 return found
+#             else:
+#                 target.pop()
