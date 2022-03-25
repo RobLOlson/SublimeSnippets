9b15c5921f481c4811f3011721a557aaa3b972f6
Rob L Olson <rob.louis@gmail.com>
2022-03-25 08:47:26 -0800

    improve a few snippets

--- select_to_scope.py
+++ select_to_scope.py
@@ -5,14 +5,24 @@ import sublime_plugin
 
 _MEMORY = []
 
-# Renamed "NextModification" from "BetterExpandScope"
-# Why?  So that LSP's expand will default here
-# Because 'ctrl+.' normally points at "next_modification"
+# !! Renamed "NextModification" from "BetterExpandScope"
+# !! Why?  So that LSP's expand will default here
+# !! Because 'ctrl+.' normally points at "next_modification"
 class NextModificationCommand(sublime_plugin.TextCommand):
     def split_scope(self, pos):
         return re.findall(r"(?:^|\s+|\.)[^\s.]+", self.view.scope_name(pos))
 
     def find_by_selector_containing(self, selector, point):
+        """Finds a region of text matching selector and containtaining point.
+
+        Args:
+            selector: a semantic description of the cursor's location
+                (e.g., 'python.python-meta.function')
+            point: the index of the editing cursor
+
+        Returns:
+            A <sublime.Region> matching selector and containing point.
+        """
         return next(
             region
             for region in self.view.find_by_selector(selector)
@@ -22,10 +32,10 @@ class NextModificationCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         selection = self.view.sel()
         new_lines_q = False
+
+        # region indeces needed in order to place cursor at start of region
         a_i = selection[0].a
         b_i = selection[0].b
-
-        # global _MEMORY
 
         for region in selection:
             _MEMORY.append(region)
@@ -61,6 +71,18 @@ class NextModificationCommand(sublime_plugin.TextCommand):
                 selection.add(sublime.Region(selection[0].a, selection[0].b))
 
     def get_expanded(self, region):
+        """Returns a <sublime.Region> that is strictly larger than the one passed in.
+
+        Args:
+            region: a small <sublime.Region>
+
+        Returns:
+            A larger <sublime.Region>
+
+        Examples:
+            >>> get_expanded(sublime.Region(100,200))
+            <sublime.Region(99,201)>
+        """
         target = self.split_scope(region.begin())  # starting index of passed selection
         while len(target):
             found = self.find_by_selector_containing("".join(target), region.begin())
@@ -68,85 +90,6 @@ class NextModificationCommand(sublime_plugin.TextCommand):
                 return found
             else:
                 target.pop()
-
-                class NextModificationCommand(sublime_plugin.TextCommand):
-                    def split_scope(self, pos):
-                        return re.findall(
-                            r"(?:^|\s+|\.)[^\s.]+", self.view.scope_name(pos)
-                        )
-
-                    def find_by_selector_containing(self, selector, point):
-                        return next(
-                            region
-                            for region in self.view.find_by_selector(selector)
-                            if region.contains(point)
-                        )
-
-                    def run(self, edit):
-                        selection = self.view.sel()
-                        new_lines_q = False
-                        a_i = selection[0].a
-                        b_i = selection[0].b
-
-                        print(f"{self.__class__}")
-
-                        for region in selection:
-                            expanded = self.get_expanded(region)
-                            if expanded and "\n" not in self.view.substr(expanded):
-                                selection.add(sublime.Region(expanded.b, expanded.a))
-                            else:
-                                new_lines_q = True
-
-                        if new_lines_q:
-                            selection.add(
-                                sublime.Region(selection[0].b, selection[0].a)
-                            )
-
-                            # If selection is not bounded by brackets
-                            if (
-                                "{"
-                                not in self.view.substr(self.view.selection[0].a - 1)
-                                and "("
-                                not in self.view.substr(self.view.selection[0].a - 1)
-                                and "["
-                                not in self.view.substr(self.view.selection[0].a - 1)
-                            ):
-                                self.view.run_command(
-                                    "expand_selection", {"to": "indentation"}
-                                )
-                            self.view.run_command(
-                                "expand_selection", {"to": "brackets"}
-                            )
-                            selection.add(
-                                sublime.Region(selection[0].b, selection[0].a)
-                            )
-
-                            if abs(a_i - b_i) == abs(selection[0].a - selection[0].b):
-                                self.view.run_command(
-                                    "move",
-                                    {
-                                        "by": "stops",
-                                        "empty_line": True,
-                                        "extend": True,
-                                        "forward": False,
-                                    },
-                                )
-                                selection.add(
-                                    sublime.Region(selection[0].a, selection[0].b)
-                                )
-
-                    def get_expanded(self, region):
-                        target = self.split_scope(
-                            region.xpos
-                        )  # starting index of passed selection
-                        while len(target):
-                            found = self.find_by_selector_containing(
-                                "".join(target), region.begin()
-                            )
-                            if found.contains(region) and not region.contains(found):
-                                return found
-                            else:
-                                target.pop()
 
 
 class NarrowSelectionCommand(sublime_plugin.TextCommand):
@@ -164,20 +107,25 @@ class NarrowSelectionCommand(sublime_plugin.TextCommand):
         selection = self.view.sel()
 
         if _MEMORY:
-            memory = _MEMORY.pop()
+            old_selection = []
+            for region in selection:
+                memory = _MEMORY.pop()
+
+                old_selection.append(memory)
 
             selection.clear()
-            selection.add(memory)
+            for region in old_selection:
+                selection.add(region)
 
         else:
             for region in selection:
-                expanded = self.get_expanded(region)
+                expanded = self.get_narrow(region)
                 # print(f"{expanded}")
                 if expanded:
                     selection.clear()
                     selection.add(sublime.Region(expanded.a, expanded.b))
 
-    def get_expanded(self, region):
+    def get_narrow(self, region):
         point = region.begin() + int(region.size() / 2)
         target = self.split_scope(point)  # middle index of passed selection
         prev = region
@@ -187,9 +135,7 @@ class NarrowSelectionCommand(sublime_plugin.TextCommand):
                 prev = found
                 target.pop()
             else:
-                print(f"{found=}")
                 return prev
-                return found
 
 
 # Original Version
