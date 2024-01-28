8e1000e3e74f772661058d46246631b15e6aa0ac
Rob Olson <rob.louis@gmail.com>
2024-01-28 06:43:44 -0900

    fix run_on_save.py

--- run_on_save.py
+++ run_on_save.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import sublime
 import sublime_plugin
 
@@ -6,19 +8,14 @@ class AutoRunPytestOnSave(sublime_plugin.EventListener):
     """A class to listen for events triggered by ST."""
 
     def on_post_save_async(self, view):
-        """
-        This is called after a view has been saved. It runs in a separate thread
-        and does not block the application.
-        """
+        """Run pytest for the current *.py file when it is saved."""
 
-        file_path = view.file_name()
-        if not file_path:
+        file_path = Path(view.file_name())
+
+        if not file_path or file_path.suffix != ".py":
             return
-        NOT_FOUND = -1
-        pos_dot = file_path.rfind(".")
-        if pos_dot == NOT_FOUND:
-            return
-        file_extension = file_path[pos_dot:]
-        if file_extension.lower() in [".py"]:
-            # view.window().run_command("eslint")
-            view.window().run_command("exec", {"cmd": "py -m pytest -s"})
+
+        test_path = file_path.parent / "tests" / f"test_{file_path.name}"
+
+        if test_path.exists():
+            view.window().run_command("exec", {"cmd": f"py -m pytest -s {test_path}"})
